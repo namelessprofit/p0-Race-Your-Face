7342863f52799589acdc9d9c48f7ea1c25141ef6
namelessprofit <posadaarchy@gmail.com>
2017-03-03 16:32:00 -0800

    added logic to move player 1 across the screen. added the same logic to move player 2 however it is not moving. I see the console loggin my margin is increasing but the image does not move.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log("connected!");
+// A $( document ).ready() block.
+$(document).ready(function() {
+    console.log("ready!");
+
+    var $player1 = $('#player1');
+    var $player2 = $('#player2');
+    var moveRight = 0;
+    var moveLeft = 0;
+
+    $('body').keydown(function(e) {
+        e.preventDefault();
+        if (e.which === 39) {
+            console.log("in key code 39");
+            moveRight += 50;
+            console.log(moveRight.toString());
+            $player1.css('margin-left', '+=50');
+        } else if (e.which === 37) {
+            console.log("in key code 37");
+            moveLeft += 50
+            console.log(moveLeft.toString());
+            $player2.css('margin-left', '+=50');
+        }
+    });
+
+})

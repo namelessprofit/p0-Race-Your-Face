afeb2dde430c624982dd5cc9d11af517beeeb1d7
namelessprofit <posadaarchy@gmail.com>
2017-03-05 13:08:20 -0800

    added event listenter to try to call the winner funtcion if the key presses result in 1200px, but that was also unsuccessful

--- js/app.js
+++ js/app.js
@@ -6,8 +6,9 @@ $(document).ready(function() {
     var $player2 = $('#player2');
     var moveRight = 0;
     var moveLeft = 0;
-//fucntion to move player 1 right
+//fucntion to move player 1 & 2 right
     $('body').keydown(function(e) {
+//conditional statement to move player 1 right
         if (e.which === 39) {
             e.preventDefault();
             moveRight += 50;
@@ -20,23 +21,17 @@ $(document).ready(function() {
             $player2.css('margin-left', '+=50');
         }
     })
-//fucntion to declare winner
-var winner = function() {
-    if ($player1.css.marginleft === 1200) {
+//function to declare winner
+var winner = $('body').keydown(function(e) {
+    if (e.moveRight >= 1200) {
     console.log("Player 1 wins!");
-} else if ($player1.css.marginleft === 1200) {
+} else if ($player1.css.marginleft >= 1200) {
     console.log("Player 2 wins!");
 }
-}
-function resetGame() {
-document.getElementById('reload');
-console.log("new game!");
-}
+})
+});
 //function to reset game
-// function resetGame() {
-// document.getElementById('reload');
-// // $('#reload').click(function() {
-// //     location.reload();
-// console.log("new game!");
-// }
+$('#reload').click(function() {
+    location.reload();
+console.log("new game!");
 });

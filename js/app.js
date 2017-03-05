aad1f2cc4d8d86a2a90e0089e189761f461ab98d
namelessprofit <posadaarchy@gmail.com>
2017-03-05 12:15:07 -0800

    added comments to code and added a button to reset the game. It is currently not operational. There are no syntax errors according to Dev Tools. Winner function is also not operational.Currently working on a fix.

--- js/app.js
+++ js/app.js
@@ -6,39 +6,31 @@ $(document).ready(function() {
     var $player2 = $('#player2');
     var moveRight = 0;
     var moveLeft = 0;
-
+//fucntion to move player 1 right
     $('body').keydown(function(e) {
         if (e.which === 39) {
             e.preventDefault();
             moveRight += 50;
             $player1.css('margin-left', '+=50');
         }
+//conditional statement to move player 2 right
         if (e.which === 37) {
             e.preventDefault();
             moveLeft += 50
             $player2.css('margin-left', '+=50');
         }
-        var winner = (function() {
-            if (($player1.css)
-                'margin-left' === 1250)) {
-            console.log("Player 1 wins!");
-
-        } else if ($player2.css 'margin-left' === 1250) {
-            console.log("Player 2 wins!");
-        }
-    });
-})
-
-// var winner = (function() {
-//     if (($player1.css)
-//         'margin-left' === 1250)) {
-//     console.log("Player 1 wins!");
-//
-// } else if ($player2.css 'margin-left' === 1250) {
-//     console.log("Player 2 wins!");
-// }
-// }
-// });
-
-// if $player1.css('margin-left' === 1250) {
-// console.log("Player 1 wins!");
+    })
+//fucntion to declare winner
+var winner = function() {
+    if ($player1.css.marginleft === 1200) {
+    console.log("Player 1 wins!");
+} else if ($player1.css.marginleft === 1200) {
+    console.log("Player 2 wins!");
+}
+}
+});
+//function to reset game
+$('#reload').click(function() {
+    location.reload();
+console.log("new game!");
+});
